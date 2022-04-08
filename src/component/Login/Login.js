cde2ff94951a676d2e89988acacc022584a0b11c
Shaker Ahamed <ahamedshaker7@gmail.com>
2022-04-08 15:46:44 +0600

    forum section done

---
+++ src/component/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div className=''>
+            <h4 className='text-center p-3'>Please Login</h4>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/component/Register/Register.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Register.css'
+
+const Register = ({ btn }) => {
+    return (
+        <div className=''>
+            <h4 className='text-center p-3'>Please Register</h4>
+        </div>
+    );
+};
+
+export default Register;
